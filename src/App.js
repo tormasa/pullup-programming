9d698182463174f8e14dd034b99c1b5cfa061685
tormasa <matti.a.torvinen@gmail.com>
2022-07-06 14:53:11 +0300

    setup gh-pages deployment add first button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	const handleTestClick = () => {
+		setCount(count + 1);
+	};
+
 	return (
 		<div className="App">
-			<p>lol</p>
+			<p>lol {count}</p>
+
+			<Button variant="primary" onClick={handleTestClick}>Test Result</Button>{' '}
 		</div>
 	);
 }
